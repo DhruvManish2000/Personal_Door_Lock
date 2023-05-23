d655452784690529ce8ed496b5bea8840fe92ba3
Max Eberle <xam7600@gmail.com>
2023-05-23 12:53:13 -0600

    Project setup.

---
+++ Digital Door Lock.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Max Eberle and Dhruv Pranlal
+ *
+ * Created on May 23, 2023, 12:46 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
